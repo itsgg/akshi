2cb83476e09fe304adde387e50cac973b849399c
Ganesh Gunasegaran <me@itsgg.com>
2019-04-23 09:55:56 +0530

    Cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   root to: 'subjects#index'
-  get 'users', to: 'users#new'
 
   resources :subjects
   resources :users
 
   get 'signup', to: 'users#new', as: 'signup'
+  post 'signup', to: 'users#create'
+
   get 'login', to: 'sessions#new', as: 'login'
   post 'login', to: 'sessions#create'
+
   delete 'logout', to: 'sessions#destroy', as: 'logout'
 end
