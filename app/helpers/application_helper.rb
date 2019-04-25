f9f2b2f9fefacd027ad83e0c69fdca6b9f043f82
Ganesh Gunasegaran <me@itsgg.com>
2019-04-25 14:46:10 +0530

    Added menu active helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,13 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  def active?(type)
+    path = request.env['PATH_INFO']
+    return path == '/signup' if type == :signup
+    return path == '/login' if type == :login
+    return path == '/subjects/new' if type == :subjects_new
+    return path.include?('/admin') if type == :admin
+    return path == '/admin' if type == :admin_root
+    return path == '/admin/users' if type == :admin_users
+  end
 end
